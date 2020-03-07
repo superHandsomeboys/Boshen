aed75ec3975fc4b93fe126661af99c0862d5847e
Rachelly-h <939100032@qq.com>
2020-03-07 15:49:25 +0800

    黄炜亿新增html和css+js

--- boshen/src/main/java/com/gpnu/boshen/config/MvcConfig.java
+++ boshen/src/main/java/com/gpnu/boshen/config/MvcConfig.java
@@ -2,9 +2,15 @@ package com.gpnu.boshen.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -13,27 +19,45 @@ public class MvcConfig implements WebMvcConfigurer {
     public WebMvcConfigurer webMvcConfigurer() {
         WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
 
-            //首页轮播图加载路径
+            /*//首页轮播图加载路径
             private String INDEX_UPLOAD_FOLDER = "///E:/boshen/demos/news/images/carousel/";
             //关于我们轮播图加载路径
-            private String ABOUT_UPLOAD_FOLDER = "///E:/boshen/images/about/";
+            private String ABOUT_UPLOAD_FOLDER = "///E:/boshen/images/about/";*/
 
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/index").setViewName("index");
+                registry.addViewController("/index").setViewName("首页.html");
+                registry.addViewController("/test").setViewName("test.html");
             }
 
-            //            拦截器
-           /* @Override
+            /*//            拦截器
+            @Override
             public void addInterceptors(InterceptorRegistry registry) {
-                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html", "/", "/user/login");
+                registry.addInterceptor(new HandlerInterceptor() {
+                    @Override
+                    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                        return false;
+                    }
+
+                    @Override
+                    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+                    }
+
+                    @Override
+                    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+                    }
+                }).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html", "/", "/user/login", "/index", "/textboxio/**", "/static/**" );
             }*/
 
-            @Override
+            /*@Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("/index/slider/**").addResourceLocations("file:" + INDEX_UPLOAD_FOLDER);
                 registry.addResourceHandler("/about/slider/**").addResourceLocations("file:" + ABOUT_UPLOAD_FOLDER);
-            }
+            }*/
+
         };
         return webMvcConfigurer;
     }
--- boshen/src/main/java/com/gpnu/boshen/controller/SliderController.java
+++ boshen/src/main/java/com/gpnu/boshen/controller/SliderController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -23,7 +21,7 @@ public class SliderController {
 
         model.addAttribute("sliders", sliders);
 
-        return "index";
+        return "新闻发布页面";
     }
 
 //    @GetMapping("/dept")
